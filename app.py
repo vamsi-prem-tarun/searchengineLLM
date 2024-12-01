d928cf6cc3b9009a5e48f028c6416872844a49bb
vamsi prem tarun <75713006+vamsi-prem-tarun@users.noreply.github.com>
2024-12-01 12:41:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ wiki=WikipediaQueryRun(api_wrapper=api_wrapper_wiki)
 api_wrapper_arxiv=ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=250)
 arxiv=ArxivQueryRun(api_wrapper=api_wrapper_arxiv)
 
-search=DuckDuckGoSearchRun(name="Search")
+# search=DuckDuckGoSearchRun(name="Search")
 
 
 st.title("Langchain- chat with search")
@@ -50,7 +50,7 @@ if prompt:=st.chat_input(placeholder="what is machine learning?"):
     st.chat_message("user").write(prompt)
 
     llm=ChatGroq(groq_api_key=groq_api_key,model_name="Llama3-8b-8192",streaming=True)
-    tools=[search,arxiv,wiki]
+    tools=[arxiv,wiki]
     search_agent=initialize_agent(tools=tools,llm=llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
     with st.chat_message("assistant"):
         st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
